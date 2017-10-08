d41411274cd7b761b1fea6caa5bde2a30da41de7
Alex <ahansen2@trinity.edu>
2017-10-08 17:09:08 -0500

    parallelized fitness calculations

--- genetic_painting/src/main.rs
+++ genetic_painting/src/main.rs
@@ -9,7 +9,7 @@ use rsgenetic::sim::*;
 use rsgenetic::sim::select::*;
 
 fn main() {
-    let mut population = (0..40).map(|_| Painting::informed_random("redgreenfuzzy.bmp", 300)).collect();
+    let mut population = (0..40).map(|_| Painting::informed_random("trump8.jpg", 400)).collect();
     let mut s = Simulator::builder(&mut population)
                     .set_selector(Box::new(MaximizeSelector::new(10)))
                     .set_max_iters(25)

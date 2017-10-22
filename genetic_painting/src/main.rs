ce177f1c28c4abccd35644c91147969dab347921
sezna <alex@alex-hansen.com>
2017-10-22 13:54:24 -0500

    from leviosa

--- genetic_painting/src/main.rs
+++ genetic_painting/src/main.rs
@@ -10,12 +10,12 @@ use rsgenetic::sim::*;
 use rsgenetic::sim::select::*;
 
 fn main() {
-    let mut population:Vec<Painting> = (0..500).map(|_| Painting::informed_random("image.png", 2000)).collect();
+    let mut population:Vec<Painting> = (0..9000).map(|_| Painting::informed_random("image.jpg", 4000)).collect();
     population[0].render_painting("sample.png");
     population[1].render_painting("sample2.png");
     let mut s = Simulator::builder(&mut population)
-                    .set_selector(Box::new(MaximizeSelector::new(10)))
-                    .set_max_iters(50)
+                    .set_selector(Box::new(StochasticSelector::new(10)))
+                    .set_max_iters(5000)
                     .build();
     s.run();
     println!("the most fit member is: {}", s.get().unwrap().fitness());
--- genetic_painting/src/stroke/mod.rs
+++ genetic_painting/src/stroke/mod.rs
@@ -84,7 +84,9 @@ impl Painting {
 //           Vec<u8>>>(rendered_strokes_buffer, stroke.start, stroke.end, stroke.color);
 
 
-            draw_line_segment_mut(&mut rendered_strokes_buffer, (stroke.start.x as f32, stroke.start.y as f32), (stroke.end.x as f32, stroke.end.y as f32), stroke.color);
+	    for i in 0..stroke.width {
+            	draw_line_segment_mut(&mut rendered_strokes_buffer, (stroke.start.x as f32 + i as f32, stroke.start.y as f32 + i as f32), (stroke.end.x as f32 + i as f32, stroke.end.y as f32), stroke.color);
+	    }
             /*
             let slope: f64 = (stroke.end.y as f64 - stroke.start.y as f64) /
                              (stroke.end.x as f64 - stroke.start.x as f64);

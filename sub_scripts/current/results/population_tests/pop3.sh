d3255e2e485eb8e2417bb643cfd91b9b3ccb9d52
sezna <alex@alex-hansen.com>
2018-03-06 10:29:14 -0600

    new results, cleaned old files

--- sub_scripts/current/results/population_tests/pop3.sh
+++ sub_scripts/current/results/population_tests/pop3.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ##Place PBS directives here
-#PBS -N pop3
+#PBS -N pop4
 #PBS -l nodes=1:ppn=36
 #PBS -l walltime=72:00:00
 #PBS -M ahansen2@trinity.edu
 #PBS -m ae
 
 ##Place Linux commands to run on the remote node here
-time ~/thesis/genetic_painting/target/release/genetic_painting --file 'image.jpg' --iterations 5000 --population 5000 --selector parmaximize --strokes 5000 --strokewidth 20 -v 1 -m 5 -M 300  --maxcurve 5
+time ~/thesis/genetic_painting/target/release/genetic_painting --file 'image.jpg' --iterations 100 --population 120 --selector parmaximize --strokes 5000 --strokewidth 20 -v 1 -m 5 -M 300  --maxcurve 5

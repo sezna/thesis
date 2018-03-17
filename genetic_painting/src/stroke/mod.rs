8386f3362915ea27067f7fb46f501b786b7caec8
sezna <ahansen2@trinity.edu>
2018-03-17 15:15:33 -0500

    increased mutation strength

--- genetic_painting/src/stroke/mod.rs
+++ genetic_painting/src/stroke/mod.rs
@@ -283,28 +283,33 @@ impl Phenotype<i32> for Painting {
     fn mutate(&self) -> Painting {
         let mut rng = thread_rng();
         let mut s = self.clone();
-        let to_modify_index = rng.gen::<usize>() % self.strokes.len();
-        let mut to_modify = self.strokes[to_modify_index].clone();
 
-        // Decide which part of the stroke we will modify.
-        match rng.gen::<i32>() % 3 {
-            0 => {
-                to_modify.start.x = (to_modify.start.x + rng.gen::<u32>() % 5) % self.width;
-                to_modify.start.y = (to_modify.start.y + rng.gen::<u32>() % 5) % self.height;
-            }
-            1 => {
-                to_modify.end.x = (to_modify.end.x + rng.gen::<u32>() % 5) % self.width;
-                to_modify.end.y = (to_modify.end.y + rng.gen::<u32>() % 5) % self.height;
-            }
-            2 => {
-                to_modify.width = to_modify.width + rng.gen::<u32>() % 5;
-            }
-            _ => (),
+	for _ in 0..10 {
+		// Decide which stroke to modify.
+		let to_modify_index = rng.gen::<usize>() % self.strokes.len();
+		let mut to_modify = self.strokes[to_modify_index].clone();
 
-        }
+		
+		// Decide which part of the stroke to modify.
+		match rng.gen::<i32>() % 3 {
+		    0 => {
+			to_modify.start.x = (to_modify.start.x + rng.gen::<u32>() % 30) % self.width;
+			to_modify.start.y = (to_modify.start.y + rng.gen::<u32>() % 30) % self.height;
+		    }
+		    1 => {
+			to_modify.end.x = (to_modify.end.x + rng.gen::<u32>() % 30) % self.width;
+			to_modify.end.y = (to_modify.end.y + rng.gen::<u32>() % 30) % self.height;
+		    }
+		    2 => {
+			to_modify.width = to_modify.width + rng.gen::<u32>() % 30;
+		    }
+		    _ => (),
 
-        s.strokes.remove(to_modify_index);
-        s.strokes.push(to_modify);
+		}
+
+		s.strokes.remove(to_modify_index);
+		s.strokes.push(to_modify);
+	}
     	return s;
     }
 }

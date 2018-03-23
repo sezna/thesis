d65def76dc9d04dc6560e819b12aaa2f97588abd
sezna <ahansen2@trinity.edu>
2018-03-23 17:36:47 -0500

    tree visualization

--- huffman/huffman_data_context/src/main.rs
+++ huffman/huffman_data_context/src/main.rs
@@ -1,10 +1,9 @@
-
 /// A node for the DataContext.
 #[derive(Clone)]
 enum Node {
     Interior {
-        left: Option<Box<Node>>,
-        right: Option<Box<Node>>,
+        left: Box<Node>,
+        right: Box<Node>,
         benefit: u64,
     },
     Leaf { data: String, benefit: u64 },
@@ -22,7 +21,7 @@ impl Node {
         match self {
             &Node::Leaf { ref data, .. } => {
                 if data == token {
-                    Some("0".to_string())
+                    Some("".to_string())
                 } else {
                     None
                 }
@@ -30,6 +29,16 @@ impl Node {
             &Node::Interior { .. } => self.handle_interior_traversal(token), 
         }
     }
+
+		fn children(&self) -> String {
+				return match self {
+								&Node::Interior { ref left, ref right, .. }=> format!("Node: [l: {} || r: {} ]", left.children(), right.children()),
+								&Node::Leaf { ref data, .. }  => format!("data: {}", data)
+
+				}
+				
+		}
+		
 
     fn handle_interior_traversal(&self, token: &str) -> Option<String> {
         let mut to_return = "".to_string();
@@ -42,10 +51,10 @@ impl Node {
                 // TODO: These expects will cause a crash. Need to just return
                 // none if there is no left. Ideally, they should all be balanced
                 // in a Huffman though?
-                if let Some(result) = left.clone().expect("").traverse(token) {
+                if let Some(result) = left.clone().traverse(token) {
                     to_return = format!("0{}", result)
                 };
-                if let Some(result) = right.clone().expect("").traverse(token) {
+                if let Some(result) = right.clone().traverse(token) {
                     to_return = format!("1{}", result)
                 };
             }
@@ -56,9 +65,56 @@ impl Node {
         if to_return == "".to_string() {
             return None;
         } else {
+				    println!("token {}, encoded {}", token, to_return);
             return Some(to_return);
         }
     }
+
+		pub fn decode(&self, to_decode: &str) -> String {
+				let mut tree_position = self;
+				let mut to_return_string = "".to_string();
+				for direction in to_decode.chars() {
+								match direction {
+												'0' => { 
+																println!("going left");
+																match tree_position {
+																				&Node::Interior { ref left, .. } => { 
+																								tree_position = &*left;
+																				},
+
+																				&Node::Leaf { ref data, .. }=> { 
+																								println!("found data: {}", data);
+																								tree_position = self;
+																								to_return_string = format!("{} {}", to_return_string, data);
+																				}
+																}
+
+												},
+												'1' => {
+																println!("going right");
+																match self {
+																				&Node::Interior { ref right, .. } => { 
+																								tree_position = &*right;
+																				},
+
+																				&Node::Leaf { ref data, .. }=> { 
+																								println!("found data: {}", data);
+																								tree_position = self;
+																								to_return_string = format!("{} {}", to_return_string, data);
+																				}
+																}
+												},
+												_ => ()
+								}
+				}
+				return to_return_string;
+		}
+
+		pub fn visualize_tree(&self) {
+				println!("{}", self.children());
+
+
+		}
 }
 
 /// Modified Huffman code-type tree.
@@ -68,15 +124,26 @@ struct DataContext {
 }
 
 impl DataContext {
-    pub fn encode(&self, to_encode: &str) {
+    pub fn encode(&self, to_encode: &str) -> String {
         let tokens: Vec<&str> = to_encode.split(" ").collect();
+				let mut encoded_string = "".to_string();
         for token in tokens {
             let pattern = self.root.traverse(token);
             if let Some(x) = pattern {
-                println!("pattern: {}", x);
+								encoded_string = format!("{}{}", encoded_string, x);
             }
+						else {
+								println!("Something went wrong in encoding"); 
+						}
         }
+				return encoded_string;
+
     }
+
+		pub fn decode(&self, to_decode: &str) -> String {
+				return self.root.decode(to_decode);
+
+		}
 }
 
 /// Creates a DataContext based on a corpus.
@@ -114,8 +181,8 @@ fn make_context(corpus: String) -> DataContext {
 
     while forest.len() > 1 {
         let new_tree = Node::Interior {
-            left: Some(Box::new(forest[0].clone())),
-            right: Some(Box::new(forest[1].clone())),
+            left: Box::new(forest[0].clone()),
+            right: Box::new(forest[1].clone()),
             benefit: forest[0].benefit() + forest[1].benefit(),
         };
         forest.remove(1);
@@ -133,11 +200,29 @@ fn make_context(corpus: String) -> DataContext {
 }
 
 fn main() {
+
+    let corpus = String::from(
+        "this this this this test this is just a big old test I can't believe this is just a test",
+    );
+
+    let data_context = make_context(corpus);
+    let encoded = data_context.encode("test test this is a test");
+		let decoded = data_context.decode(&encoded);
+		println!("encoded: {}", encoded);
+		println!("decoded: {}", decoded);
+
+		println!("Tree Visualization\n");
+		data_context.root.visualize_tree();
+}
+
+
+#[test]
+fn test_encode() {
+				
     let corpus = String::from(
         "this this this this test this is just a big old test I can't believe this is just a test",
     );
 
     let data_context = make_context(corpus);
     data_context.encode("test test this is a test");
-
 }

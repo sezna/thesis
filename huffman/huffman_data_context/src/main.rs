72d48e7cd05f9d51f057c2c751b3380af623c8b8
sezna <ahansen2@trinity.edu>
2018-03-24 00:03:52 -0500

    encoding table

--- huffman/huffman_data_context/src/main.rs
+++ huffman/huffman_data_context/src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+// TODO: encode with encoding table
+
 /// A node for the DataContext.
 #[derive(Clone)]
 enum Node {
@@ -11,12 +15,14 @@ enum Node {
 
 
 impl Node {
+
     pub fn benefit(&self) -> u64 {
         return match self {
             &Node::Interior { benefit, .. } => benefit,
             &Node::Leaf { benefit, .. } => benefit,
         };
     }
+
     pub fn traverse(&self, token: &str) -> Option<String> {
         match self {
             &Node::Leaf { ref data, .. } => {
@@ -26,19 +32,22 @@ impl Node {
                     None
                 }
             }
-            &Node::Interior { .. } => self.handle_interior_traversal(token), 
+            &Node::Interior { .. } => self.handle_interior_traversal(token),
         }
     }
 
-		fn children(&self) -> String {
-				return match self {
-								&Node::Interior { ref left, ref right, .. }=> format!("Node: [l: {} || r: {} ]", left.children(), right.children()),
-								&Node::Leaf { ref data, .. }  => format!("data: {}", data)
+    fn children(&self) -> String {
+        return match self {
+            &Node::Interior {
+                ref left,
+                ref right,
+                ..
+            } => format!("Node: [l: {} || r: {} ]", left.children(), right.children()),
+            &Node::Leaf { ref data, .. } => format!("data: {}", data),
 
-				}
-				
-		}
-		
+        };
+
+    }
 
     fn handle_interior_traversal(&self, token: &str) -> Option<String> {
         let mut to_return = "".to_string();
@@ -48,9 +57,6 @@ impl Node {
                 ref right,
                 ..
             } => {
-                // TODO: These expects will cause a crash. Need to just return
-                // none if there is no left. Ideally, they should all be balanced
-                // in a Huffman though?
                 if let Some(result) = left.clone().traverse(token) {
                     to_return = format!("0{}", result)
                 };
@@ -65,139 +71,162 @@ impl Node {
         if to_return == "".to_string() {
             return None;
         } else {
-				    println!("token {}, encoded {}", token, to_return);
             return Some(to_return);
         }
     }
 
-		pub fn decode(&self, to_decode: &str) -> String {
-				let mut tree_position = self;
-				let mut to_return_string = "".to_string();
-				for direction in to_decode.chars() {
-								match direction {
-												'0' => { 
-																println!("going left");
-																match tree_position {
-																				&Node::Interior { ref left, .. } => { 
-																								tree_position = &*left;
-																				},
+    pub fn make_table(&self, tokens: Vec<&str>) -> HashMap<String, String> {
+        let mut to_return = HashMap::new();
+        for token in tokens {
+            println!("adding token: {}", token);
+            to_return.insert(
+                token.to_string(),
+                self.traverse(token).expect("token not found"),
+            );
 
-																				&Node::Leaf { ref data, .. }=> { 
-																								println!("found data: {}", data);
-																								tree_position = self;
-																								to_return_string = format!("{} {}", to_return_string, data);
-																				}
-																}
+        }
 
-												},
-												'1' => {
-																println!("going right");
-																match self {
-																				&Node::Interior { ref right, .. } => { 
-																								tree_position = &*right;
-																				},
+        for (token, encoding) in to_return.clone() {
+            println!("token:{}, encoding: {}", token, encoding);
+        }
+        return to_return;
 
-																				&Node::Leaf { ref data, .. }=> { 
-																								println!("found data: {}", data);
-																								tree_position = self;
-																								to_return_string = format!("{} {}", to_return_string, data);
-																				}
-																}
-												},
-												_ => ()
-								}
-				}
-				return to_return_string;
-		}
-
-		pub fn visualize_tree(&self) {
-				println!("{}", self.children());
+    }
 
 
-		}
+    pub fn decode(&self, to_decode: &str) -> String {
+        let mut tree_position = self;
+        let mut to_return_string = "".to_string();
+        for direction in to_decode.chars() {
+            match direction {
+                '0' => {
+                    //println!("going left");
+                    match tree_position {
+                        &Node::Interior { ref left, .. } => {
+                            tree_position = &*left;
+                        }
+
+                        &Node::Leaf { ref data, .. } => {
+                            //								println!("found data: {}", data);
+                            tree_position = self;
+                            to_return_string = format!("{} {}", to_return_string, data);
+                        }
+                    }
+
+                }
+                '1' => {
+                    //println!("going right");
+                    match self {
+                        &Node::Interior { ref right, .. } => {
+                            tree_position = &*right;
+                        }
+
+                        &Node::Leaf { ref data, .. } => {
+                            //println!("found data: {}", data);
+                            tree_position = self;
+                            to_return_string = format!("{} {}", to_return_string, data);
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+        return to_return_string;
+    }
+
+    pub fn visualize_tree(&self) {
+        println!("{}", self.children());
+
+
+    }
 }
 
 /// Modified Huffman code-type tree.
 struct DataContext {
     _context_id: String,
     root: Node,
+    encoding_table: HashMap<String, String>,
 }
 
 impl DataContext {
+
     pub fn encode(&self, to_encode: &str) -> String {
         let tokens: Vec<&str> = to_encode.split(" ").collect();
-				let mut encoded_string = "".to_string();
+        let mut encoded_string = "".to_string();
         for token in tokens {
             let pattern = self.root.traverse(token);
             if let Some(x) = pattern {
-								encoded_string = format!("{}{}", encoded_string, x);
+                encoded_string = format!("{}{}", encoded_string, x);
+            } else {
+                println!("Something went wrong in encoding");
             }
-						else {
-								println!("Something went wrong in encoding"); 
-						}
         }
-				return encoded_string;
+        return encoded_string;
 
     }
 
-		pub fn decode(&self, to_decode: &str) -> String {
-				return self.root.decode(to_decode);
+    pub fn decode(&self, to_decode: &str) -> String {
+        return self.root.decode(to_decode);
 
-		}
-}
+    }
 
-/// Creates a DataContext based on a corpus.
-fn make_context(corpus: String) -> DataContext {
-    // Gather vector of pointers to individual words in the corpus.
-    let tokens: Vec<&str> = corpus.split(" ").collect();
+    /// Creates a DataContext based on a corpus.
+    fn new(corpus: String) -> DataContext {
+        // Gather vector of pointers to individual words in the corpus.
+        let mut tokens: Vec<&str> = corpus.split(" ").collect();
 
-    // Create unique tuples of tokens and their benefit.
-    let mut tokens_with_benefit: Vec<(&str, u64)> = tokens
-        .iter()
-        .map(|x| {
-            (
-                *x,
-                (tokens.iter().filter(|&y| y == x).count() * x.len()) as u64,
-            )
-        })
-        .collect();
-    tokens_with_benefit.push(("token not contained", 0u64));
-    tokens_with_benefit.sort();
-    tokens_with_benefit.dedup();
-    tokens_with_benefit.sort_by_key(|x| x.1);
+        // Create unique tuples of tokens and their benefit.
+        let mut tokens_with_benefit: Vec<(&str, u64)> = tokens
+            .iter()
+            .map(|x| {
+                (
+                    *x,
+                    (tokens.iter().filter(|&y| y == x).count() * x.len()) as u64,
+                )
+            })
+            .collect();
+        tokens_with_benefit.push(("token not contained", 0u64));
+        tokens_with_benefit.sort();
+        tokens_with_benefit.dedup();
+        tokens_with_benefit.sort_by_key(|x| x.1);
 
-    // Create the Huffman tree. At this point, tokens_with_benefit is sorted
-    // by lowest benefit to highest benefit.
-    let mut forest: Vec<Node> = tokens_with_benefit
-        .iter()
-        .map(|x| {
-            Node::Leaf {
-                data: x.0.to_string(),
-                benefit: x.1,
-            }
-        })
-        .collect();
-    forest.sort_by_key(|x| x.benefit());
-
-    while forest.len() > 1 {
-        let new_tree = Node::Interior {
-            left: Box::new(forest[0].clone()),
-            right: Box::new(forest[1].clone()),
-            benefit: forest[0].benefit() + forest[1].benefit(),
-        };
-        forest.remove(1);
-        forest.remove(0);
-        forest.push(new_tree);
+        // Create the Huffman tree. At this point, tokens_with_benefit is sorted
+        // by lowest benefit to highest benefit.
+        let mut forest: Vec<Node> = tokens_with_benefit
+            .iter()
+            .map(|x| {
+                Node::Leaf {
+                    data: x.0.to_string(),
+                    benefit: x.1,
+                }
+            })
+            .collect();
         forest.sort_by_key(|x| x.benefit());
 
-    }
+        while forest.len() > 1 {
+            let new_tree = Node::Interior {
+                left: Box::new(forest[0].clone()),
+                right: Box::new(forest[1].clone()),
+                benefit: forest[0].benefit() + forest[1].benefit(),
+            };
+            forest.remove(1);
+            forest.remove(0);
+            forest.push(new_tree);
+            forest.sort_by_key(|x| x.benefit());
 
-    assert!(forest.len() == 1);
-    return DataContext {
-        _context_id: "Test".to_string(),
-        root: forest[0].clone(),
-    };
+        }
+
+        tokens.sort();
+        tokens.dedup();
+        assert!(forest.len() == 1);
+        return DataContext {
+            _context_id: "Test".to_string(),
+            root: forest[0].clone(),
+            encoding_table: forest[0].clone().make_table(tokens),
+        };
+    }
 }
+
 
 fn main() {
 
@@ -205,20 +234,20 @@ fn main() {
         "this this this this test this is just a big old test I can't believe this is just a test",
     );
 
-    let data_context = make_context(corpus);
+    let data_context = DataContext::new(corpus);
     let encoded = data_context.encode("test test this is a test");
-		let decoded = data_context.decode(&encoded);
-		println!("encoded: {}", encoded);
-		println!("decoded: {}", decoded);
+    let decoded = data_context.decode(&encoded);
+    println!("encoded: {}", encoded);
+    println!("decoded: {}", decoded);
 
-		println!("Tree Visualization\n");
-		data_context.root.visualize_tree();
+    println!("Tree Visualization\n");
+    data_context.root.visualize_tree();
 }
 
 
 #[test]
 fn test_encode() {
-				
+
     let corpus = String::from(
         "this this this this test this is just a big old test I can't believe this is just a test",
     );
